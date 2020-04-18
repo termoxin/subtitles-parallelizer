ced3d0fa9cff8b76f915b1ee31b786c0f7903914
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-04-18 12:01:23 +0300

    Refactor: code example

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import path from "path";
+
 import { getFileContent } from "./helpers/filesystem";
 import { getSections } from "./helpers/subtitles";
 import { glueStringsBy } from "./helpers/array";
@@ -6,16 +7,17 @@ import { glueStringsBy } from "./helpers/array";
 const [start, end] = process.argv.slice(2);
 
 const run = async () => {
-  const enSubtitles = getFileContent(path.resolve(__dirname, "../en_2.srt"));
-  const ruSubtitles = getFileContent(path.resolve(__dirname, "../ru_2.srt"));
-
-  const [en, ru] = await Promise.all([enSubtitles, ruSubtitles]);
+  const [en, ru] = await Promise.all([
+    getFileContent(path.resolve(__dirname, "../en.srt")),
+    getFileContent(path.resolve(__dirname, "../ru.srt")),
+  ]);
 
   const [englishTranscript, russianTranscript] = getSections({
-    firstLang: en,
-    secondLang: ru,
     start,
     end,
+    firstLang: en,
+    secondLang: ru,
+    secondLangSplitter: "\r\n",
   });
 
   console.log(
