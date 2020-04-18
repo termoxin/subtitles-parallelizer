6d64b66268fe52fb7fba32798f271cc3995a72fc
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-04-18 14:46:45 +0300

    Refactor: remove example code

--- src/index.js
+++ src/index.js
@@ -1,31 +1,4 @@
-import path from "path";
-
-import { getFileContent } from "./helpers/filesystem";
-import { getSections } from "./helpers/subtitles";
-import { glueStringsBy } from "./helpers/array";
-
-const [start, end] = process.argv.slice(2);
-
-const run = async () => {
-  const [en, ru] = await Promise.all([
-    getFileContent(path.resolve(__dirname, "../en.srt")),
-    getFileContent(path.resolve(__dirname, "../ru.srt")),
-  ]);
-
-  const [englishTranscript, russianTranscript] = getSections({
-    start,
-    end,
-    firstLang: en,
-    secondLang: ru,
-    secondLangSplitter: "\r\n",
-  });
-
-  console.log(
-    `${glueStringsBy(englishTranscript, "content")}\n\n${glueStringsBy(
-      russianTranscript,
-      "content"
-    )}`
-  );
-};
-
-run();
+export { getSections, getSectionByWord } from "./helpers/subtitles";
+export { glueStringsBy } from "./helpers/array";
+export { getFileContent } from "./helpers/filesystem";
+export { srtTimeToSeconds } from "./helpers/time";
