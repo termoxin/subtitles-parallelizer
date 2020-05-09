a04b800e0aa44d5575420af8d99196d54bd574e5
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-09 12:30:33 +0300

    Refactor: remove redundant function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ export {
   getSectionsByWord,
   getTranscript,
 } from "./helpers/subtitles";
+
 export { glueStringsBy } from "./helpers/array";
-export { getFileContent } from "./helpers/filesystem";
 export { srtTimeToSeconds } from "./helpers/time";
