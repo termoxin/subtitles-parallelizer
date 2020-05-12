1248fbcf38b36f4ede4a5def8f46359bb6315328
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-12 19:06:33 +0300

    Refactor: replace compose helper by in-house one

--- src/sections/parse.js
+++ src/sections/parse.js
@@ -1,5 +1,5 @@
-import { compose } from "ramda";
 import { stripTags } from "../helpers/general/string";
+import { compose } from "../helpers/general/fp";
 import { splitOnObjectSections } from "./splitOnObjectSections";
 
 /**
