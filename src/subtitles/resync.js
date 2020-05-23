b17499d08a9a7fcd23777b9a151064cfe890f689
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-23 12:42:45 +0300

    Docs: for resync

--- src/subtitles/resync.js
+++ src/subtitles/resync.js
@@ -1,5 +1,38 @@
 import { secondsToSrtTime, srtTimeToSeconds, withoutMs } from "./index";
 
+/**
+ * The function resynchonizes subtitles
+ *
+ * @param subtitles - array of objects that ``parse`` function produces
+ * @param time - offset time (you can use negative value for flexibity)
+ *
+ * @example
+ * ```js
+ *
+  * const unresyncedSubtitles = [
+      {
+        id: 1,
+        content: "a lot of text",
+        startTime: "00:00:26",
+        endTime: "00:00:29",
+        startTimeWithMs: "00:00:26,500",
+        endTimeWithMs: "00:00:29,461",
+      },
+      {
+        id: 2,
+        content: "a lot of text",
+        startTime: "00:00:29",
+        endTime: "00:00:36",
+        startTimeWithMs: "00:00:29,500",
+        endTimeWithMs: "00:00:36,461",
+      },
+  ];
+
+ *
+ * resync(unresyncedSubtitles, 2000)
+ * ```
+ */
+
 export const resync = (subtitles, time) =>
   subtitles.map(({ startTimeWithMs, endTimeWithMs, ...props }) => {
     const resyncedStartTimeWithMs = secondsToSrtTime(
