f3beed4065437b8949d311665b4931caa11cc70e
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-23 12:42:58 +0300

    Docs: minor correction

--- src/sections/parse.js
+++ src/sections/parse.js
@@ -3,7 +3,7 @@ import { compose } from "../helpers/general/fp";
 import { splitOnObjectSections } from "./splitOnObjectSections";
 
 /**
- * The function takes subtitles in srt format and returns sections as array of objects
+ * The function takes subtitles text in srt format and returns sections as array of objects
  * @param text - subtitles text to parse into array of objects
  *
  * @example
