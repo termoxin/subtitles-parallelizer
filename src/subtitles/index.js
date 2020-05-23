e24a133aa64528c2d4475556dd015a4e06ca3134
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-23 12:24:51 +0300

    Feature: create resync function

--- src/subtitles/index.js
+++ src/subtitles/index.js
@@ -54,3 +54,22 @@ export const createPreviousAndNextSec = (time) => {
 
   return [previousSec, nextSec];
 };
+
+export const resync = (subtitles, time) =>
+  subtitles.map(({ startTimeWithMs, endTimeWithMs, ...props }) => {
+    const resyncedStartTimeWithMs = secondsToSrtTime(
+      srtTimeToSeconds(startTimeWithMs) + time / 1000
+    );
+
+    const resyncedEndTimeWithMs = secondsToSrtTime(
+      srtTimeToSeconds(endTimeWithMs) + time / 1000
+    );
+
+    return {
+      ...props,
+      startTimeWithMs: resyncedStartTimeWithMs,
+      endTimeWithMs: resyncedEndTimeWithMs,
+      startTime: withoutMs(resyncedStartTimeWithMs),
+      endTime: withoutMs(resyncedEndTimeWithMs),
+    };
+  });
