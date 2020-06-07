d05675b57d56fe8654e7cd83e531f5fb5e551047
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-06-07 10:09:20 +0300

    Refactor: export resync helper

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { parseByName } from "./sections/parseByName";
 export { parseByTimestamp } from "./sections/parseByTimestamp";
 export { parseBoth } from "./sections/parseBoth";
 export { srtTimeToSeconds, secondsToSrtTime } from "./subtitles/index";
+export { resync } from "./subtitles/resync";
