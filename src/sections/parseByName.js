3f089cf7c3229f1cfd673329dbfa24ce34afd84c
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-06-23 21:00:12 +0300

    Refactor: parseByName to make it works with offset third argument

--- src/sections/parseByName.js
+++ src/sections/parseByName.js
@@ -1,4 +1,5 @@
 import { parse } from "./parse";
+import { getBetweenBy } from "../helpers/general/array";
 
 /**
  * The function takes name and subtitles text to find a word or a phrase in each section's content of the subtitles
@@ -9,5 +10,23 @@ import { parse } from "./parse";
  * @returns the same array of objects as like ``parse`` function does
  */
 
-export const parseByName = (name, text) =>
-  parse(text).filter((value) => new RegExp(name, "gi").test(value.content));
+export const parseByName = (name, text, offset) => {
+  if (offset) {
+    const parsedSubtitles = parse(text);
+
+    const { id } = parsedSubtitles.filter((value) =>
+      new RegExp(name, "gi").test(value.content)
+    )[0];
+
+    return getBetweenBy(
+      parsedSubtitles,
+      id - offset.left,
+      id + offset.right,
+      "id"
+    );
+  }
+
+  return parse(text).filter((value) =>
+    new RegExp(name, "gi").test(value.content)
+  );
+};
