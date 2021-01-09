2622b6297fdf9647a3e0efcccf0c3711d4ff3fec
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-01-09 09:11:32 +0200

    Test: cover one more use case for more complicated strings

--- test/formatSection.test.js
+++ test/formatSection.test.js
@@ -12,7 +12,6 @@ test("should return formatted string", () => {
     mistaken or exercise he on although.
     Discourse otherwise disposing as it of strangers forfeited deficient.
   `;
-
   const formattedTextSection = `
     Stronger unpacked felicity to of mistaken. Fanny at wrong table ye in.
     Be on easily cannot innate in lasted months on.
@@ -21,6 +20,33 @@ test("should return formatted string", () => {
     Raptures stanhill my greatest mistaken or exercise he on although.
     Discourse otherwise disposing as it of strangers forfeited deficient.
   `;
+
+  expect(formatSection(textSection)).toEqual(formattedTextSection.trim());
+});
+
+test("should format complicated string and return formatted one", () => {
+  const textSection = `You haven't gone
+to visit Dad yet?
+We're taking things one
+step at a time, girl.
+If we don't visit, how are
+we supposed to forgive him?
+What are you talking about?
+Elizabeth, at the farm, they taught us
+forgiveness is the most valuable gift.
+If we don't absolve him, we
+become what he was. Full of hate.
+My babies deserve a mother
+who's better than that.
+`;
+
+  const formattedTextSection = `You haven't gone to visit Dad yet?
+We're taking things one step at a time, girl.
+If we don't visit, how are we supposed to forgive him?
+What are you talking about?
+Elizabeth, at the farm, they taught us forgiveness is the most valuable gift.
+If we don't absolve him, we become what he was. Full of hate.
+My babies deserve a mother who's better than that.`;
 
   expect(formatSection(textSection)).toEqual(formattedTextSection.trim());
 });
